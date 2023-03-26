3f4ac428d83639ef34c29ee6c1591ffd3929255a
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-03-26 12:42:41 -0300

    feat: iteração + condicional

--- basic/03-estruturasDeControle3.rb
+++ basic/03-estruturasDeControle3.rb
@@ -28,3 +28,33 @@ end
 10.times do
     puts 'Hello'
 end
+
+#ITERAÇÃO + CONDICIONAL
+
+result = ''
+loop do 
+    puts result
+    puts 'Selecione uma das seguintes opções'
+    puts '1 - Descobrir idade da pessoa'
+    puts '0 - Sair'
+    print 'Opção: '
+
+    option = gets.chomp.to_i
+
+    if option == 1
+        print 'Digite o ano de nascimento: '
+        anoNascimento = gets.chomp.to_i
+
+        print 'Digite o ano atual: '
+        anoAtual = gets.chomp.to_i
+
+        idade = anoAtual - anoNascimento
+
+        result = "Quem nasceu no ano de #{anoNascimento}, vai ter #{idade} anos em #{anoAtual}"
+
+    elsif option == 0
+        break if option == 0
+    else 
+        result = 'Opção invalida'
+    end
+end
