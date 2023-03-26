666dd9f9049303929a2c9ec2d68bd6aa50b1242f
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-03-26 12:25:44 -0300

    for while do

---
+++ basic/03-estruturasDeControle3.rb
@@ -0,0 +1,25 @@
+#FOR
+fruits = ['Maçã', 'Uva', 'Morango']
+
+for fruit in fruits
+    puts fruit
+end
+
+#WHILE
+x = 1
+
+while x <= 10
+    puts x
+    x += 1
+end
+
+#LOOP
+count = 1
+loop do
+    puts count 
+    # break if count == 10
+    if count == 10
+        break 
+    end
+    count += 1
+end
