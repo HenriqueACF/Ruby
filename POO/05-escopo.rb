9a96266a908c55e4cdd26520748ff3e2988689b3
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-03-29 10:23:42 -0300

    escopo de variaveis

---
+++ POO/05-escopo.rb
@@ -0,0 +1,16 @@
+# VARIAVEL LOCAL
+# pode ser acessada apenas onde foi criada
+
+# VARIAVEL GLOBAL
+# pode ser acessada em qualquer lugar do programa
+# é declarada com o prefixo $
+
+# VARIAVEL DE CLASSE
+# pode ser acessada em qualquer lugar da classe
+# é declarada com o prefixo @@
+
+# VARIAVEL DE INSTANCIA
+# semelhante a variavel de classe, tendo a diferença de que o valor nao é compartilhado entre todas as instancias de sua classe
+# é declarado com o prefixo @
+
+
