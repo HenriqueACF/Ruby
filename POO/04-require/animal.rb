d99de7b7d00183aa3c401dcc8b79e604d68a7a1c
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-03-29 10:16:37 -0300

    require

---
+++ POO/04-require/animal.rb
@@ -0,0 +1,11 @@
+class Animal
+  def pular
+    puts 'Animal Pulou'
+  end
+
+  def dormir
+    puts 'Animal Dormiu'
+  end
+end
+
+
---
+++ POO/04-require/app.rb
@@ -0,0 +1,8 @@
+# REQUIRE -> LEVA EM CONTA ONDE ESTOU
+# REQUIRE_RELATIVE -> LEVA EM CONTA ONDE ESTA O ARQUIVO SENDO EXECUTADO
+require './animal.rb'
+# require_relative 'animal'
+animal = Animal.new
+animal.dormir
+animal.pular
+
