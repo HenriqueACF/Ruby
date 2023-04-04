8a641b98caaf3b4db05e30f8d6a443f4efcdc3fa
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-04-04 02:48:23 -0300

    feat: create and migrate db

--- RubyOnRails/tour_of_heroes_api/db/migrate/20230404053924_create_heros.rb
+++ RubyOnRails/tour_of_heroes_api/db/migrate/20230404053924_create_heros.rb
@@ -1,7 +1,7 @@
 class CreateHeros < ActiveRecord::Migration[6.1]
   def change
     create_table :heros do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
