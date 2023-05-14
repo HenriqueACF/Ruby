fba394e850aba8c14a26e31f6792f75d7fb6ce04
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-05-14 12:11:55 -0300

    feat(Order): Create order strategy

--- urubu-do-pix/app.rb
+++ urubu-do-pix/app.rb
@@ -7,10 +7,15 @@ require 'dotenv'
 Dotenv.load
 
 require './app/adapters/controllers/users_controllers'
+require './app/adapters/controllers/orders_controller'
 
 set :database, { adapter: 'sqlite3', database: ENV['DB_PATH']}
 
 post '/users' do
   {result: UsersController.create(params)}.to_json
+end
+
+post '/orders' do
+  {result: OrdersController.create(params)}.to_json
 end
 
---
+++ urubu-do-pix/app/adapters/repositories/order_repository.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+class OrderRepository < ActiveRecord::Base
+  self.table_name = 'orders'
+end
